4f567e96ae92fa659a27235aa9effed95d016d96
Stjushenka <stjusheka@gmail.com>
2021-12-03 13:59:07 +0300

    Calc

---
+++ src/ServiceCalc.java
@@ -0,0 +1,13 @@
+public class ServiceCalc {
+    public float a;
+    public float p;
+    public float t;
+    public float calculate(float a, float p, float t) {
+        this.a = a;
+        this.p = p;
+        this.t = t;
+        float monthPay;
+        monthPay = (float) (a * p / 1200 / (1 - Math.pow(1 + p / 1200, -t)));
+        return monthPay;
+    }
+}
