b568771db3db79ae669f6c384d9d1f93a563971b
Stjushenka <stjusheka@gmail.com>
2021-12-07 15:14:49 +0300

    Calc

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         ServiceCalc service = new ServiceCalc();
         float monthPay;
-        float a = 1000000;
+        float a = 1000000f;
         float p = 9.9f;
-        float t = 12;
+        float t=12f;
         monthPay = service.calculate(a, p, t);
         System.out.println(monthPay);
     }
